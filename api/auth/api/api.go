127f6c592c170834f468ad4fad58c57e00c33f28
oliverharris <thejuggernaut@harriso.co.uk>
2021-03-14 19:43:53 +0000

    update errors

--- api/auth/api/api.go
+++ api/auth/api/api.go
@@ -2,9 +2,9 @@ package api
 
 import (
 	"io/ioutil"
-	"log"
 	"net/http"
 
+	"github.com/ProjectOrangeJuice/golib-logging/logging"
 	"github.com/projectorangejuice/Discordv2/db"
 )
 
@@ -12,15 +12,13 @@ import (
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	responseData, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Printf("Failed to read body for username in login handler, %s", err)
-		http.Error(w, "Failed to read body", http.StatusInternalServerError)
+		logging.LogHTTP("loginHandler", "Failed to read body", err, w)
 		return
 	}
 
 	ok, err := db.DoesUserExist(string(responseData))
 	if err != nil {
-		log.Printf("Failed to check username, %s", err)
-		http.Error(w, "Failed to read body", http.StatusInternalServerError)
+		logging.LogHTTP("LoginHandler", "Failed to check username", err, w)
 		return
 	}
 
--- api/auth/go.mod
+++ api/auth/go.mod
@@ -3,6 +3,7 @@ module github.com/projectorangejuice/Discordv2
 go 1.14
 
 require (
+	github.com/ProjectOrangeJuice/golib-logging v0.0.0-20210314194145-150d82cead53
 	github.com/gorilla/mux v1.8.0
 	github.com/mattn/go-sqlite3 v1.14.6
 )
